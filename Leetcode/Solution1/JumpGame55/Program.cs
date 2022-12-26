63ee0efcbc2bbce608ea4eee9ae127e52047222e
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2022-12-27 00:26:21 +0500

    55. Jump Game

---
+++ Leetcode/Solution1/JumpGame55/Program.cs
@@ -0,0 +1,22 @@
+﻿bool CanJump()
+{
+    var nums = new int[] { 2, 3, 1, 1, 4 };
+
+    var maxIndex = 0;
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+        if (i <= maxIndex)
+        {
+            maxIndex = Math.Max(maxIndex, i + nums[i]);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return maxIndex >= nums.Length - 1;
+}
+
+Console.WriteLine(CanJump());
