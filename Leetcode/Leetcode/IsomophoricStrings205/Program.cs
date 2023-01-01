a87383afb9b8ac4440f1a31ab0f82dc8a93e4b00
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-01 20:52:17 +0500

    205. Isomorphic Strings

---
+++ Leetcode/Leetcode/IsomophoricStrings205/Program.cs
@@ -0,0 +1,35 @@
+﻿bool IsIsomorphic(string s, string t)
+{
+    if (s.Length != t.Length) return false;
+
+    var dict = new Dictionary<char, char>();
+    var length = s.Length;
+
+    for (var i = 0; i < length; i++)
+    {
+        var a = s[i];
+        var b = t[i];
+
+        if (dict.ContainsValue(b))
+        {
+            var c = dict.First(d => d.Value == b).Key;
+
+            if(c != a) return false;
+        }
+
+        if (dict.ContainsKey(a))
+        {
+            if (dict[a] != b) return false;
+        }
+        else
+        {
+            dict.Add(a, b);
+        }
+    }
+
+    return true;
+}
+
+Console.WriteLine(IsIsomorphic("egg", "add"));
+Console.WriteLine(IsIsomorphic("foo", "bar"));
+Console.WriteLine(IsIsomorphic("paper", "title"));
