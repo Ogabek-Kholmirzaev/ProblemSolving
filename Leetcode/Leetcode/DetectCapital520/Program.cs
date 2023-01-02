8619c88395979d5fcb3b594183d6070c2362a32e
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-02 10:41:47 +0500

    520. Detect Capital

---
+++ Leetcode/Leetcode/DetectCapital520/Program.cs
@@ -0,0 +1,17 @@
+﻿bool DetectCapitalUse(string word)
+{
+    var cnt = word.Count(char.IsUpper);
+
+    if (cnt == word.Length || cnt == 0)
+        return true;
+
+    if (cnt == 1 && char.IsUpper(word[0]))
+        return true;
+
+    return false;
+}
+
+Console.WriteLine(DetectCapitalUse("USA"));
+Console.WriteLine(DetectCapitalUse("FlaG"));
+Console.WriteLine(DetectCapitalUse("Flag"));
+Console.WriteLine(DetectCapitalUse("g"));
