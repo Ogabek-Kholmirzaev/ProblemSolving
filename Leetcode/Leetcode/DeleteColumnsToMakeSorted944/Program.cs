5f03767bdedf808e408a53f9586b016e2ef59dd3
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-03 15:38:32 +0500

    944.  Delete Columns to Make Sorted

---
+++ Leetcode/Leetcode/DeleteColumnsToMakeSorted944/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+int MinDeletionSize(string[] strs)
+{
+    var cnt = 0;
+    var n = strs.Length;
+    var m = strs[0].Length;
+
+    for (var i = 0; i < m; i++)
+    {
+        var word = new StringBuilder();
+
+        for (var j = 0; j < n; j++)
+            word.Append(strs[j][i]);
+
+        var wordCharArray = word.ToString().ToCharArray();
+
+        Array.Sort(wordCharArray);
+
+        if(word.ToString() != new string(wordCharArray))
+            cnt++;
+    }
+
+    return cnt;
+}
+
+Console.WriteLine(MinDeletionSize(new string[] { "cba", "daf", "ghi" }));
