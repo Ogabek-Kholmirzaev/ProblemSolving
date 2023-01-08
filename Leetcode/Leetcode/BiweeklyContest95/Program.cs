87c0779019b46231fa5a45490a4cb123d5e27a7c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-08 14:05:35 +0500

    95. Biweekly Contest

---
+++ Leetcode/Leetcode/BiweeklyContest95/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello World!");
+
+public class DataStream {
+    private readonly int _value;
+    private readonly int _k;
+    private List<int> nums;
+
+    public DataStream(int value, int k) {
+        _value = value;
+        _k = k;
+        nums = new List<int>();
+    }
+    
+    public bool Consec(int num) {
+        nums.Add(num);
+        if (nums.Count < _k)
+            return false;
+        
+        for(var i = nums.Count - 1; i >= nums.Count - _k; i--)
+            if (nums[i] != _value)
+            {
+                return false;
+            }
+
+        return true;
+    }
+}
