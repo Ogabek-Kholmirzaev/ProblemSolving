dc34d9a5dcfbadaa7b46c19efbb17a26b37bf06c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-09 00:10:44 +0500

    2521. Distinct Prime Factors of Product of Array

---
+++ Leetcode/Leetcode/DistinctPrimeFactorsOfProduct2521/Program.cs
@@ -0,0 +1,28 @@
+﻿int DistinctPrimeFactors(int[] nums)
+{ 
+    var primes = new HashSet<int>();
+    for (var i = 0; i < nums.Length; i++)
+    {
+	    var num = nums[i];
+	    var k = 2;
+
+	    while (num != 1)
+	    {
+		    if (num % k == 0)
+		    {
+			    primes.Add(k);
+			    while (num % k == 0)
+			    {
+				    num /= k;
+			    }
+		    }
+
+		    k++;
+	    }
+    }
+
+    return primes.Count;
+}
+
+Console.WriteLine(DistinctPrimeFactors(new []{2,4,3,7,10,6}));
+Console.WriteLine(DistinctPrimeFactors(new []{2,4,8,16}));
