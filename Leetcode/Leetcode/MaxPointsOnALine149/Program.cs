f504a35137ee0ab4f82132486af661810cede043
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-08 23:52:56 +0500

    149. Max Points on a Line

---
+++ Leetcode/Leetcode/MaxPointsOnALine149/Program.cs
@@ -0,0 +1,28 @@
+﻿int MaxPoints(int[][] points) {
+    int n = points.Length;
+
+    if (n <= 2) return n;
+
+    int ans = 2;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int total = 2;
+            for (int k = 0; k < n; k++) {
+                if (k == i || k == j) continue;
+                if ((points[j][1] - points[i][1]) * (points[i][0] - points[k][0]) == (points[i][1] - points[k][1]) * (points[j][0] - points[i][0])) {
+                    total++;
+                }
+            }
+            ans = Math.Max(ans, total);
+        }
+    }
+    return ans;
+}
+
+Console.WriteLine(MaxPoints(new []
+{
+    new []{1, 1},
+    new []{2, 2},
+    new []{3, 3}
+}));
