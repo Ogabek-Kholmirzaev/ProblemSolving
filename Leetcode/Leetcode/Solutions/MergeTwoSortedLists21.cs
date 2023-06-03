759b3faf451233f45cbe03513c575319bc8daf07
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-06-04 01:50:26 +0500

    21. Merge Two Sorted Lists

---
+++ Leetcode/Leetcode/Solutions/MergeTwoSortedLists21.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.Solutions;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val; 
+        this.next = next;
+    }
+}
+
+public class MergeTwoSortedLists21
+{
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 == null)
+            return list1;
+
+        if (list2 == null)
+            return list1;
+
+        if (list1.val <= list2.val)
+        {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else
+        {
+            list2.next = MergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
