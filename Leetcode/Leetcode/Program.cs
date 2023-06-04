d4c1fcc16a479ed325a01533cb61b6946274824c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-06-04 16:42:05 +0500

    Daily Leetcode Chellenge 574. Number of Provinces

--- Leetcode/Leetcode/Program.cs
+++ Leetcode/Leetcode/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Leetcode.Solutions;
+
+var solution = new NumberofProvinces547();
+
+Console.WriteLine(solution.FindCircleNum(new int[][]
+{
+    new []{1, 1, 0},
+    new []{1, 1, 0},
+    new []{0, 0, 1}
+}));
+
+Console.WriteLine(solution.FindCircleNum(new int[][]
+{
+    new []{1, 0, 0},
+    new []{0, 1, 0},
+    new []{0, 0, 1}
+}));
+
+Console.WriteLine(solution.FindCircleNum(new int[][]
+{
+    new []{ 1, 0, 0, 1 },
+    new []{ 0, 1, 1, 0 },
+    new []{ 0, 1, 1, 1 },
+    new []{ 1, 0, 1, 1 }
+}));
---
+++ Leetcode/Leetcode/Solutions/NumberofProvinces547.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.Solutions;
+
+public class NumberofProvinces547
+{
+    public List<int> Cities;
+
+    public int FindCircleNum(int[][] isConnected)
+    {
+        Cities = new List<int>();
+        var provinces = 0;
+
+        for (var i = 0; i < isConnected.Length; i++)
+        {
+            if (!Cities.Contains(i))
+            {
+                Cities.Add(i);
+                Solve(i, isConnected);
+                provinces++;
+            }
+        }
+
+        return provinces;
+    }
+
+    public void Solve(int cityNumber, int[][] isConnected)
+    {
+        for (var i = 0; i < isConnected.Length; i++)
+        {
+            if (isConnected[cityNumber][i] == 1 && !Cities.Contains(i))
+            {
+                Cities.Add(i);
+                Solve(i, isConnected);
+            }
+        }
+    }
+}
