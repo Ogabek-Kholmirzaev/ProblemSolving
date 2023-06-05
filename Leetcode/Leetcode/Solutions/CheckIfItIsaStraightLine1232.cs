d0db772b76a6386901c3f67a797d57a8882eb6fd
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-06-05 21:58:46 +0500

    1232. Check If It is a Straight Line

---
+++ Leetcode/Leetcode/Solutions/CheckIfItIsaStraightLine1232.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.Solutions;
+
+public class CheckIfItIsaStraightLine1232
+{
+    public bool CheckStraightLine(int[][] coordinates)
+    {
+        var n = coordinates.Length;
+        var x1 = coordinates[0][0];
+        var y1 = coordinates[0][1];
+        var x2 = coordinates[1][0];
+        var y2 = coordinates[1][1];
+
+        for (var i = 0; i < n; i++)
+        {
+            var x = coordinates[i][0];
+            var y = coordinates[i][1];
+
+            if((y - y1) * (x - x2) != (y - y2) * (x - x1))
+                return false;
+        }
+
+        return true;
+    }
+}
