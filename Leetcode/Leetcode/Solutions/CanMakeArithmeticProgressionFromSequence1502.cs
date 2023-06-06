de353c790757cb0c57325759f9e385c4bc122a5d
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-06-06 22:08:06 +0500

    1502. Can Make Arithmetic Progression From Sequence

---
+++ Leetcode/Leetcode/Solutions/CanMakeArithmeticProgressionFromSequence1502.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.Solutions;
+
+public class CanMakeArithmeticProgressionFromSequence1502
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+        Array.Sort(arr);
+
+        var dif = arr[1] - arr[0];
+
+        for (var i = 2; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != dif)
+                return false;
+        }
+
+        return true;
+    }
+}
