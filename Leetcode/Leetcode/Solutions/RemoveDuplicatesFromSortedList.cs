9b71406bf357a5043aaf36950cda2e306c8ae40b
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-26 13:27:15 +0500

    83. Remove Duplicates from Sorted List

---
+++ Leetcode/Leetcode/Solutions/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,24 @@
+﻿using Leetcode.Solutions.Models;
+
+namespace Leetcode.Solutions;
+
+public class RemoveDuplicatesFromSortedList
+{
+    public static ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head == null)
+            return head;
+
+        var current = head;
+
+        while (current.next != null)
+        {
+            if (current.val == current.next.val)
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+
+        return head;
+    }
+}
