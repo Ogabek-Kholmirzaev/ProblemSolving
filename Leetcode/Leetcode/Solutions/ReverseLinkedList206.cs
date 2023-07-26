7cb590ae90a79b23a1bbcbe95484ce41efc09cc9
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-26 15:11:23 +0500

    203. Remove Linked List Elements 206. Reverse Linked List

---
+++ Leetcode/Leetcode/Solutions/ReverseLinkedList206.cs
@@ -0,0 +1,22 @@
+﻿using Leetcode.Solutions.Models;
+
+namespace Leetcode.Solutions;
+
+public class ReverseLinkedList206
+{
+    public static ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        var current = head;
+
+        while (current != null)
+        {
+            var next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
