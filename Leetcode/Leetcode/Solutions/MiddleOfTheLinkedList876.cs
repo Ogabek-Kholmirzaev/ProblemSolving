3bdc617082e9f6272891b2bbac4ce70a0df8eb3f
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-26 16:59:05 +0500

    876. Middle of the Linked List

---
+++ Leetcode/Leetcode/Solutions/MiddleOfTheLinkedList876.cs
@@ -0,0 +1,21 @@
+﻿using Leetcode.Solutions.Models;
+
+namespace Leetcode.Solutions;
+
+public class MiddleOfTheLinkedList876
+{
+    public static ListNode MiddleNode(ListNode head)
+    {
+        var cnt = 0;
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            cnt++;
+        }
+
+        return slow;
+    }
+}
