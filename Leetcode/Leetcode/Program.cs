a7d9ff4ef9b1d72a8163d421c20b912548866b3b
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-27 16:18:43 +0500

    20. Valid Parentheses

--- Leetcode/Leetcode/Program.cs
+++ Leetcode/Leetcode/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Leetcode.Solutions;
 
-Console.WriteLine(PeakIndexInAMountainArray852.PeakIndexInMountainArray(new[] { 0, 1, 0 }));
-Console.WriteLine(PeakIndexInAMountainArray852.PeakIndexInMountainArray(new[] { 0, 2, 1, 0 }));
-Console.WriteLine(PeakIndexInAMountainArray852.PeakIndexInMountainArray(new[] { 0, 10, 5, 2 }));
-Console.WriteLine(PeakIndexInAMountainArray852.PeakIndexInMountainArray(new[] { 3, 5, 3, 2, 0 }));
+Console.WriteLine(ValidParentheses20.IsValid("()"));
+Console.WriteLine(ValidParentheses20.IsValid("()[]{}"));
+Console.WriteLine(ValidParentheses20.IsValid("()"));
+Console.WriteLine(ValidParentheses20.IsValid("(]"));
---
+++ Leetcode/Leetcode/Solutions/ValidParentheses20.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode.Solutions;
+
+public class ValidParentheses20
+{
+    public static bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        var bracketsDictionary = new Dictionary<char, char>()
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+        foreach (var item in s)
+        {
+            if (bracketsDictionary.ContainsValue(item))
+            {
+                stack.Push(item);
+                continue;
+            }
+
+            if (stack.Count == 0 || stack.Pop() != bracketsDictionary[item])
+                return false;
+        }
+
+        return stack.Count == 0;
+    }
+}
