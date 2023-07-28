df95e7e681c3339461667367bf8746e2a68fe34a
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-29 01:02:09 +0500

    2384. Largest Palindromic Number

--- Leetcode/Leetcode/Program.cs
+++ Leetcode/Leetcode/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Leetcode.Solutions;
 
-Console.WriteLine(LongestPalindrome409.LongestPalindrome("abccccdd"));
-Console.WriteLine(LongestPalindrome409.LongestPalindrome("a"));
+Console.WriteLine(LargestPalindromicNumber2384.LargestPalindromic("444947137"));
+Console.WriteLine(LargestPalindromicNumber2384.LargestPalindromic("00009"));
+Console.WriteLine(LargestPalindromicNumber2384.LargestPalindromic("5736732"));
+Console.WriteLine(LargestPalindromicNumber2384.LargestPalindromic("00011"));
