5d872a92e2ad124bdaf10976a46aaac5eb89e357
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-29 17:48:24 +0500

    303. Range Sum Query - Immutable

---
+++ Leetcode/Leetcode/Solutions/RangeSumQueryImmutable303.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode.Solutions;
+
+public class RangeSumQueryImmutable303
+{
+    public class NumArray
+    {
+        private int[] sums;
+        public NumArray(int[] nums)
+        {
+            this.sums = new int[nums.Length + 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sums[i + 1] = sums[i] + nums[i];
+            }
+        }
+
+        public int SumRange(int left, int right)
+        {
+            return sums[right + 1] - sums[left];
+        }
+    }
+}
