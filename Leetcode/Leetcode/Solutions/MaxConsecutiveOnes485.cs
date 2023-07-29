e974c4ffcd3f1256c71e206dd85ddb0207fce8e3
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-29 18:41:08 +0500

    485. Max Consecutive Ones

---
+++ Leetcode/Leetcode/Solutions/MaxConsecutiveOnes485.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.Solutions;
+
+public class MaxConsecutiveOnes485
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int result = 0;
+        int current = 0;
+
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                current++;
+                continue;
+            }
+
+            result = Math.Max(result, current);
+            current = 0;
+        }
+
+        return Math.Max(result, current);
+    }
+}
