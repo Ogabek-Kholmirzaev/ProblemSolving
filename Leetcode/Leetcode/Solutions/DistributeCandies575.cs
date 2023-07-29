219348a9689b48e4753111b5c8dc22b343592976
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-29 18:55:20 +0500

    575. Distribute Candies

---
+++ Leetcode/Leetcode/Solutions/DistributeCandies575.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Leetcode.Solutions;
+
+public class DistributeCandies575
+{
+    public int DistributeCandies(int[] candyType)
+    {
+        var dict = new Dictionary<int, bool>();
+
+        foreach (var candy in candyType)
+        {
+            if (!dict.ContainsKey(candy))
+            {
+                dict.Add(candy, true);
+            }
+        }
+
+        return Math.Min(dict.Count, candyType.Length / 2);
+    }
+}
