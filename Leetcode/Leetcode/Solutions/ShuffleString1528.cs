750101f0a9674aa5c6630b98cc147c814336122a
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-07-30 17:01:26 +0500

    1528. Shuffling String

---
+++ Leetcode/Leetcode/Solutions/ShuffleString1528.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Leetcode.Solutions;
+
+public class ShuffleString1528
+{
+    public static string RestoreString(string s, int[] indices)
+    {
+        var result = new StringBuilder(s);
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            result[indices[i]] = s[i];
+        }
+
+        return result.ToString();
+    }
+}
