e5e4dde2da71e4be718f6dbb37aeb9d906515c6e
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-08-03 23:00:51 +0500

    2367. Number of Arithmetic Triplets

---
+++ Leetcode/Leetcode/NumberOfArithmethicTriplets2367.cs
@@ -0,0 +1,31 @@
+﻿namespace Leetcode;
+
+public class NumberOfArithmethicTriplets2367
+{
+    /// <summary>
+    /// 2367. Number of Arithmetic Triplets => Leetcode
+    /// </summary>
+    /// <param name="nums">Strictly increasing integer array</param>
+    /// <param name="diff"></param>
+    /// <returns></returns>
+    public static int ArithmeticTriplets(int[] nums, int diff)
+    {
+        var cnt = 0;
+        var dict = new Dictionary<int, bool>();
+
+        foreach (var num in nums)
+        {
+            dict.Add(num, true);
+        }
+
+        foreach (var dictItem in dict)
+        {
+            if (dict.ContainsKey(dictItem.Key + 2 * diff) && dict.ContainsKey(dictItem.Key + diff))
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
