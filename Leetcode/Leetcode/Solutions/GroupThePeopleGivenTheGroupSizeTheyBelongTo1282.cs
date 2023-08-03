e5983cb4336794bb60e6f12c76193b95ce57852a
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-08-03 20:01:04 +0500

    1282. Group the People Given the Group Size They Belong To

---
+++ Leetcode/Leetcode/Solutions/GroupThePeopleGivenTheGroupSizeTheyBelongTo1282.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode.Solutions;
+
+public class GroupThePeopleGivenTheGroupSizeTheyBelongTo1282
+{
+    public static IList<IList<int>> GroupThePeople(int[] groupSizes)
+    {
+        var result = new List<IList<int>>();
+        var groupsDict = new Dictionary<int, List<int>>();
+
+        for(int i = 0; i < groupSizes.Length; i++)
+        {
+            if (!groupsDict.ContainsKey(groupSizes[i]))
+            {
+                groupsDict.Add(groupSizes[i], new List<int>() { i });
+                continue;
+            }
+
+            groupsDict[groupSizes[i]].Add(i);
+        }
+
+        foreach(var group in groupsDict)
+        {
+            var groupSize = group.Key;
+            var groupMembers = group.Value;
+
+            for(int i = 0; i < groupMembers.Count; i += groupSize)
+            {
+                result.Add(groupMembers.GetRange(i, groupSize));
+            }
+        }
+
+        return result;
+    }
+}
