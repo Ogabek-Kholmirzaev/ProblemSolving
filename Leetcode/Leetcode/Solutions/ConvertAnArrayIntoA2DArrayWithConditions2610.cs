44d390a8e8eace19ff2e4e8a3f5cd9a6dec881c9
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-08-03 22:42:44 +0500

    2610. Convert an Array Into a 2D Array With Conditions

---
+++ Leetcode/Leetcode/Solutions/ConvertAnArrayIntoA2DArrayWithConditions2610.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode.Solutions;
+
+public class ConvertAnArrayIntoA2DArrayWithConditions2610
+{
+    public static IList<IList<int>> FindMatrix(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        var dict = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                dict[num]++;
+                continue;
+            }
+
+            dict.Add(num, 1);
+        }
+
+        while (dict.Count > 0)
+        {
+            var row = new List<int>();
+
+            foreach (var dictItem in dict)
+            {
+                row.Add(dictItem.Key);
+                dict[dictItem.Key]--;
+
+                if (dict[dictItem.Key] == 0)
+                {
+                    dict.Remove(dictItem.Key);
+                }
+            }
+
+            result.Add(row);
+        }
+
+        return result;
+    }
+}
