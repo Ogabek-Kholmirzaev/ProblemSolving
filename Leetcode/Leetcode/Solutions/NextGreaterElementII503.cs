d1fc9f8cc2a1043f559408b900df719f9639d624
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-08-12 17:34:40 +0500

    739. Daily Temperatures => Error

---
+++ Leetcode/Leetcode/Solutions/NextGreaterElementII503.cs
@@ -0,0 +1,43 @@
+﻿namespace Leetcode.Solutions;
+
+public class NextGreaterElementII503
+{
+    public int[] NextGreaterElements(int[] nums)
+    {
+        var ans = new int[nums.Length];
+        var max = nums.Max();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ans[i] = -1;
+
+            if (nums[i] == max)
+            {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[j] > nums[i])
+                {
+                    ans[i] = nums[j];
+                    break;
+                }
+            }
+
+            if (ans[i] == -1)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] > nums[i])
+                    {
+                        ans[i] = nums[j];
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
