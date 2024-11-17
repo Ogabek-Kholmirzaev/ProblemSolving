d6516633a6f8a19f8d9f7945263ac175daba40c4
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-17 18:25:11 +0500

    1480. Running Sum of 1d Array

---
+++ Leetcode/TheLeetcodeBeginnersGuide/RunningSumOf1DArray.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode.TheLeetcodeBeginnersGuide;
+
+public class RunningSumOf1DArray {
+    public int[] RunningSum(int[] nums) {
+        for (int i = 1; i < nums.Length; i++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+}
