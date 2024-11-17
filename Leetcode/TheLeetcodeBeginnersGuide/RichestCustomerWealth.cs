eab1ecb71e9f83d2416638da195d3141259376bd
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-17 18:37:06 +0500

    1672. Richest Customer Wealth

---
+++ Leetcode/TheLeetcodeBeginnersGuide/RichestCustomerWealth.cs
@@ -0,0 +1,7 @@
+﻿namespace Leetcode.TheLeetcodeBeginnersGuide;
+
+public class RichestCustomerWealth {
+    public int MaximumWealth(int[][] accounts) {
+        return accounts.Select(account => account.Sum()).Max();
+    }
+}
