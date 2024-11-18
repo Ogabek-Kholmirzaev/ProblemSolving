4aba79e7465016919355502bf1c01ac09334d787
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-18 22:35:57 +0500

    412. Fizz Buzz

---
+++ Leetcode/TheLeetcodeBeginnersGuide/FizzBuzzz.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.TheLeetcodeBeginnersGuide;
+
+public class FizzBuzzz {
+    public IList<string> FizzBuzz(int n) {
+        var ans = new string[n];
+
+        for (var i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans[i - 1] = "FizzBuzz";
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                ans[i - 1] = "Fizz";
+                continue;
+            }
+
+            if (i % 5 == 0) {
+                ans[i - 1] = "Buzz";
+                continue;
+            }
+
+            ans[i - 1] = i.ToString();
+        }
+
+        return ans;
+    }
+}
