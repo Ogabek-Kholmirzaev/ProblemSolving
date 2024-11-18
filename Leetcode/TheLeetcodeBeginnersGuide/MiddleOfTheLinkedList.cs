b5e821844a2e76323d280e0042ccff2415c475fc
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-18 22:46:14 +0500

    876. Middle od the Linked List

---
+++ Leetcode/TheLeetcodeBeginnersGuide/MiddleOfTheLinkedList.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode.TheLeetcodeBeginnersGuide;
+
+public class MiddleOfTheLinkedList {
+    public ListNode MiddleNode(ListNode head) {
+        ListNode middle = head, end = head;
+
+        while (end != null && end.next != null) {
+            end = end.next.next;
+            middle = middle.next;
+        }
+
+        return middle;
+    }
+}
+
+public class ListNode { 
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
