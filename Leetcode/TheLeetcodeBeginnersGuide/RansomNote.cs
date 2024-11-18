bf2b9fd5997e143d0c236cafa7eaffe17c399e7c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-18 22:56:11 +0500

    383. Ransom Note

---
+++ Leetcode/TheLeetcodeBeginnersGuide/RansomNote.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.TheLeetcodeBeginnersGuide;
+
+public class RansomNote {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var letter in ransomNote) {
+            if (dict.ContainsKey(letter)) {
+                dict[letter]++;
+                continue;
+            }
+
+            dict.Add(letter, 1);
+        }
+        
+        foreach (var letter in magazine) {
+            if (dict.ContainsKey(letter)) {
+                dict[letter]--;
+            }
+        }
+
+        return !dict.Any(pair => pair.Value > 0);
+    }
+}
