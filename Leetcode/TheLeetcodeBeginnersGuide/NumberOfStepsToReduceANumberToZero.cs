a479985d0d0700add8244cce87320513e53199cf
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-18 22:39:10 +0500

    1342. Number of Steps to Reduce a Number to Zero

---
+++ Leetcode/TheLeetcodeBeginnersGuide/NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode.TheLeetcodeBeginnersGuide;
+
+public class NumberOfStepsToReduceANumberToZero {
+    public int NumberOfSteps(int num) {
+        int ans = 0;
+
+        while (num > 0) {
+            ans++;
+            
+            if (num % 2 == 0) {
+                num /= 2;
+                continue;
+            }
+
+            num--;
+        }
+
+        return ans;
+    }
+}
