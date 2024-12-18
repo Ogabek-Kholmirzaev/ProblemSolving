e304dc395d4541443cd98858d699cb1716183210
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-18 13:53:52 +0500

    1475. Final Prices With a Special Discount in a Shop

---
+++ Leetcode/Daily/FinalPricesWithASpecialDiscountInAShop.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode.Daily;
+
+public class FinalPricesWithASpecialDiscountInAShop
+{
+    public int[] FinalPrices(int[] prices)
+    {
+        int len = prices.Length;
+
+        for (int i = 0; i < len; i++)
+        {
+            int discount = 0;
+
+            for (int j = i + 1; j < len; j++)
+            {
+                if (prices[i] >= prices[j])
+                {
+                    discount = prices[j];
+                    break;
+                }
+            }
+
+            prices[i] -= discount;
+        }
+
+        return prices;
+    }
+}
