6e1fcc4686659d7678ed3de328ca562b013e4303
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-20 09:19:26 +0500

    769. Max Chunks To Make Sorted

---
+++ Leetcode/Daily/MaxChunksToMakeSorted.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.Daily;
+
+public class MaxChunksToMakeSorted
+{
+    public int MaxChunksToSorted(int[] arr)
+    {
+        int cnt = 0, sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum1 += arr[i];
+            sum2 += i;
+
+            if (sum1 == sum2)
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
