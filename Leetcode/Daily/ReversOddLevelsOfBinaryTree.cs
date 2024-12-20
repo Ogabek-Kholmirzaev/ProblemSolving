9cdd629bbf067e6f97d7e1e16d7bb373f085b8fc
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-20 09:44:03 +0500

    2415. Reverse Odd Levels of Binary Tree

---
+++ Leetcode/Daily/ReversOddLevelsOfBinaryTree.cs
@@ -0,0 +1,32 @@
+﻿using Leetcode.Models;
+
+namespace Leetcode.Daily;
+
+public class ReversOddLevelsOfBinaryTree
+{
+    public TreeNode ReverseOddLevels(TreeNode root)
+    {
+        dfs(root.left, root.right, 1);
+
+        return root;
+    }
+
+    private void dfs(TreeNode left, TreeNode right, int level)
+    {
+        if (left == null || right == null)
+        {
+            return;
+        }
+
+        if (level % 2 == 1)
+        {
+            int temp = left.val;
+
+            left.val = right.val;
+            right.val = temp;
+        }
+
+        dfs(left.left, right.right, level + 1);
+        dfs(left.right, right.left, level + 1);
+    }
+}
