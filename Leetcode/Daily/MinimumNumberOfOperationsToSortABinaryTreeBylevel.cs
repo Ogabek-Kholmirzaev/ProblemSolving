ed4cd67f5e0b789620891fda39a705084ffcbbb1
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-23 19:13:02 +0500

    2471. Minimum Number of Operations to Sort a Binary Tree by Level

---
+++ Leetcode/Daily/MinimumNumberOfOperationsToSortABinaryTreeBylevel.cs
@@ -0,0 +1,72 @@
+﻿using Leetcode.Models;
+
+namespace Leetcode.Daily;
+
+public class MinimumNumberOfOperationsToSortABinaryTreeBylevel
+{
+    private readonly Dictionary<int, List<int>> dict = [];
+
+    public int MinimumOperations(TreeNode root)
+    {
+        int ans = 0;
+
+        Dfs(root, 0);
+
+        foreach (var item in dict)
+        {
+            ans += MinOperationsCount(item.Value);
+        }
+
+        return ans;
+    }
+
+    private void Dfs(TreeNode node, int level)
+    {
+        if (dict.ContainsKey(level))
+        {
+            dict[level].Add(node.val);
+        }
+        else
+        {
+            dict.Add(level, [node.val]);
+        }
+
+        if (node.left != null)
+        {
+            Dfs(node.left, level + 1);
+        }
+
+        if (node.right != null)
+        {
+            Dfs(node.right, level + 1);
+        }
+    }
+
+    private int MinOperationsCount(List<int> nodeValues)
+    {
+        var sortedNodeValues = nodeValues.OrderBy(x => x).ToList();
+        var dic = new Dictionary<int, int>();
+        int cnt = 0;
+
+        for (int i = 0; i < nodeValues.Count; i++)
+        {
+            dic.Add(nodeValues[i], i);
+        }
+
+        for (int i = 0; i < nodeValues.Count; i++)
+        {
+            if (nodeValues[i] != sortedNodeValues[i])
+            {
+                cnt++;
+
+                int temp = dic[sortedNodeValues[i]];
+
+                dic[nodeValues[i]] = temp;
+                nodeValues[temp] = nodeValues[i];
+                nodeValues[i] = sortedNodeValues[i];
+            }
+        }
+
+        return cnt;
+    }
+}
