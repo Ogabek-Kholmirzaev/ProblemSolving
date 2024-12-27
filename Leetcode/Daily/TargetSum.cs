803892882ec359ae69877d27d2b79d2770f10b98
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-27 22:03:28 +0500

    494. Target Sum

---
+++ Leetcode/Daily/TargetSum.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode.Daily;
+
+public class TargetSum
+{
+    private int ans = 0;
+
+    public int FindTargetSumWays(int[] nums, int target)
+    {
+        Calc(nums, target, 0, 0);
+
+        return ans;
+    }
+
+    private void Calc(int[] nums, int target, int sum, int index)
+    {
+        if (index == nums.Length)
+        {
+            if (sum == target)
+            {
+                ans++;
+            }
+        }
+        else
+        {
+            Calc(nums, target, sum + nums[index], index + 1);
+            Calc(nums, target, sum - nums[index], index + 1);
+        }
+    }
+}
