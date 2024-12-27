7d827f38c658dac5bbe2d2cad80e7f308f817802
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-27 22:08:31 +0500

    1014. Best Sightseeing Pair

---
+++ Leetcode/Daily/BestSightseeingPair.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode.Daily;
+
+public class BestSightseeingPair
+{
+    public int MaxScoreSightseeingPair(int[] values)
+    {
+        int res = 0;
+        int maxLeft = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            res = Math.Max(res, maxLeft + values[i] - i);
+            maxLeft = Math.Max(maxLeft, values[i] + i);
+        }
+
+        return res;
+    }
+}
